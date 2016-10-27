bbc6f733aa0967057295c850c67c0bb9f2995b58
Ross Hamilton <rosshamilton.dev@gmail.com>
2016-10-28 00:55:03 +0100

    Red-Green-Refactor by Example

--- spec.rb
+++ spec.rb
@@ -6,16 +6,42 @@ class Person
     @last_name = last_name
   end
 
-  # implement your behavior here
+  def full_name
+    @middle_name ? "#{@first_name} #{@middle_name} #{@last_name}" : "#{@first_name} #{@last_name}"
+  end
+
+  def full_name_with_middle_initial
+    "#{@first_name} #{@middle_name[0]} #{@last_name}"
+  end
+
+  def initials
+    "#{@first_name[0]} #{@middle_name[0]} #{@last_name[0]}"
+  end
 end
 
 RSpec.describe Person do
   describe "#full_name" do
-    it "concatenates first name, middle name, and last name with spaces"
-    it "does not add extra spaces if middle name is missing"
+    it "concatenates first name, middle name, and last name with spaces" do
+      person = Person.new(first_name: "Korgoth", middle_name: "The", last_name: "Barbarian")
+      expect(person.full_name).to eq("Korgoth The Barbarian")
+    end
+    it "does not add extra spaces if middle name is missing" do
+      person = Person.new(first_name: "Korgoth", last_name: "Barbarian")
+      expect(person.full_name).to eq("Korgoth Barbarian")
+    end
   end
 
-  describe "#full_name_with_middle_initial"
+  describe "#full_name_with_middle_initial" do
+    it "concatenates first name, middle intial, and last name with spaces" do
+      person = Person.new(first_name: "Korgoth", middle_name: "The", last_name: "Barbarian")
+      expect(person.full_name_with_middle_initial).to eq("Korgoth T Barbarian")
+    end
+  end
 
-  describe "#initials"
+  describe "#initials" do
+    it "concatenates first initial, middle initial, and last initial with spaces" do
+      person = Person.new(first_name: "Korgoth", middle_name: "The", last_name: "Barbarian")
+      expect(person.initials).to eq("K T B")
+    end
+  end
 end
